874e3b11f0107e9a4595a102ac5e1d6d85440720
Mehrojlatipov <mehrojlatifov06@gmail.com>
2024-10-23 23:57:51 +0500

    done

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main(S, d):
     '''create a babylonian function.
-    
+
     Args:
         S (int): number
         d (int): numnber
@@ -8,4 +8,14 @@ def main(S, d):
     Returns:
         float: result
     '''
-    return
+    p = pow(d,2)
+    a = S - p / 2 * d
+
+    b = a + d
+
+    p2 = pow (a,2)
+    x = b - (p2 / 2 * b)
+    
+    return (x)
+r = main(122, 4)
+print (r)
